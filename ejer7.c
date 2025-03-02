0642dd4200d339f484131894f15b1d4643fc35dd
DiegoGC51 <diegoguerrero9361@gmail.com>
2025-03-01 23:23:08 -0600

    Create ejer7.c

---
+++ ejer7.c
@@ -0,0 +1,10 @@
+// 7. Accediendo a la Memoria
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    for (int i = 0; i < 5; i++) {
+        printf("arr[%d]: %p\n", i, &arr[i]);
+    }
+    return 0;
+}
