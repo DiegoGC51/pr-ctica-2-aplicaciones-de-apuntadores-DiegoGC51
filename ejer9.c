d551618f91327936c87afac0cdb420f2735936ab
DiegoGC51 <diegoguerrero9361@gmail.com>
2025-03-01 23:24:51 -0600

    Create ejer9.c

---
+++ ejer9.c
@@ -0,0 +1,18 @@
+// 9. Suma con Apuntadores
+#include <stdio.h>
+
+int sumaArreglo(int *arr, int tamaño) {
+    int suma=0;
+    for (int i=0; i<tamaño; i++) {
+        suma += *(arr + i);
+    }
+    return suma;
+}
+
+int main() {
+    int numeros[]= {1, 2, 3, 4, 5};
+    int tamaño=sizeof(numeros) / sizeof(numeros[0]);
+    int resultado=sumaArreglo(numeros, tamaño);
+    printf("La suma de los elementos del arreglo es: %d\n", resultado);
+    return 0;
+}
